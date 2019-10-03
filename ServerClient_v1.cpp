559d602c0aa0076591a711e8177d462d747e0bfd
Aditya-crypto <composeforaditya@gmail.com>
2019-10-03 11:56:26 +0530

    second commit

--- ServerClient_v1.cpp
+++ ServerClient_v1.cpp
@@ -5,77 +5,131 @@
 #include <arpa/inet.h>
 #include<unistd.h>
 #include<errno.h>
-#define QUE
+#include<cstdio>
+#include<stdio.h>
+#define BufferSize 512*1024
+#define PORT 5050
+#define QUE 5
 using namespace std;
 int port;
-void client()
+void* client(void* arg)
 {
-     int sp=socket(PF_INET,SOCK_STREAM,0);
+      int sp=socket(PF_INET,SOCK_STREAM,0);
     struct sockaddr_in addr;
     memset(&addr,'\0',sizeof(addr));
     addr.sin_family=AF_INET;
-    addr.sin_port=htons(port);
+    addr.sin_port=htons(PORT);
     addr.sin_addr.s_addr=inet_addr("127.0.0.1");
 
     int res =  connect(sp,(struct sockaddr*)&addr,sizeof(addr));
-        if(res<0){
-        perror("errr");
-        exit(0);}
-    char arr[10]="hello";
-    int n = send(sp,&arr,sizeof(arr),0);
-    cout<<"Sent"<<n;
-    //memset(&arr,'\0',10);
-   // n = recv(sp,&arr,3,0);
-   // cout<<"REcv"<<n;
-   // cout<<arr;
+        if(res<0)
+        {
+          perror("errorrrrrrr");
+          exit(0);
+        }
+        else
+          cout<<"connected\n";
+          int n;
+    FILE* fp=fopen("hello.txt","rb");
+    fseek(fp,0L,SEEK_END);
+     long int file_size=ftell(fp);
+    rewind(fp);
+    cout<<"client:"<<file_size<<"\n";
+    if((n=send(sp,&file_size,sizeof(file_size),0))<0)
+       perror("sending failed");
+       else
+       cout<<"sent successful"<<n<<"bytes\n";
+       char buffer[BufferSize];
+       while((n=fread(buffer,sizeof(char),BufferSize,fp))>0 && file_size>0)
+           {    cout<<"heloooo\n";
+                send(sp,buffer,n,0);
+   	 	memset(buffer,'\0',BufferSize);
+		file_size = file_size-n;
+	   }
+         cout<<"success";
+    fclose(fp);
     close(sp);
 }
 ///// server code
 void* handle_client(void* arg)
 {   
     int *t=(int*)arg;
-    int cl_fd=*t;
-    char arr[10]="";
-    recv(cl_fd,arr,sizeof(arr),0);
-    cout<<arr;
-    pthread_exit(0);
-    //return NULL;
+    int connfd=*t;
+          int n;
+          FILE *fp = fopen ("hello.txt","wb");
+          char Buffer[BufferSize] ; 
+          long int file_size;
+           n=recv(connfd, &file_size,sizeof(file_size),0);
+           if(n>0)
+             cout<<"file_size recieved\n"<<"server:"<<file_size<<"\n";
+           else
+             perror("file_size recieved failed");
+           while ( ( n=recv(connfd,Buffer,BufferSize,0) ) > 0  &&  file_size > 0)
+           {    
+                  fwrite(Buffer,sizeof(char),n,fp);
+	          memset( Buffer,'\0',BufferSize);
+                  file_size =file_size-n;
+           } 
+        fclose(fp);
+        pthread_exit(0);
+    
 }
-void server()
+void* server(void* arg)
+{
+     pthread_t tid;
+     pthread_attr_t attr;
+     pthread_attr_init(&attr);
+    int listenfd = 0, connfd = 0;
+    struct sockaddr_in serv_addr; 
+
+    char sendBuff[1025];
+    time_t ticks; 
+
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    memset(&serv_addr, '0', sizeof(serv_addr));
+    memset(sendBuff, '0', sizeof(sendBuff)); 
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_port = htons(PORT); 
+
+    bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)); 
+
+    listen(listenfd, 10); 
+    while(1)
+    {
+        connfd = accept(listenfd, (struct sockaddr*)NULL, NULL); 
+          if(connfd<0)
+              perror("not accepted");
+          if(pthread_create(&tid,&attr,handle_client,&connfd)<0)
+             perror("creation error");
+          pthread_join(tid,NULL);
+          close(connfd);
+          fflush(stdout); 
+     }
+     close(listenfd);
+}
+int main()
 {
     pthread_t tid;
     pthread_attr_t attr;
     pthread_attr_init(&attr);
-    int s_sp=socket(AF_INET,SOCK_STREAM,0);
-    struct sockaddr_in s_address,c_address;
-    s_address.sin_family=AF_INET;
-    s_address.sin_port=htons(port);
-    s_address.sin_addr.s_addr=inet_addr("127.0.0.1");
-    int length=sizeof(sockaddr);
-    if(bind(s_sp,(struct sockaddr*)&s_address,sizeof(s_address))<0)
-    perror("binding error");
-    listen(s_sp,QUE);
-   
-   while(1){
-   int c_sp=accept(s_sp,(struct sockaddr*)&c_address,(socklen_t*)&length);
-    if(c_sp<0)
-    perror("not accepted");
-    if(pthread_create(&tid,&attr,handle_client,&c_sp)<0)
-       perror("creation error");
-    pthread_join(tid,NULL);
-    close(c_sp);
-    fflush(stdout);
-   }
-    close(s_sp);
-}
-int main()
-{
     string what;
     cin>>port;
     cin>>what;
     if(what=="client")
-    client();
-    else
-    server();
+     {
+        if(pthread_create(&tid,&attr,client,NULL)<0)
+            perror("creation error");
+          pthread_join(tid,NULL);
+         
+     }
+     else
+     {
+         if(pthread_create(&tid,&attr,server,NULL)<0)
+             perror("creation error");
+         pthread_join(tid,NULL);
+         
+     }
     return 0;
 }
